348fda03470f16249a05ed8899145afac07f1134
kir <kir00@list.ru>
2024-09-24 11:03:40 +0300

    add-friends-likes_2

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.Film;
+
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
+
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.ArrayList;
